a2b7b9a230d91db643730bad39e3b6219ef7afb2
Dhanya <dhanyafab@gmail.com>
2025-04-25 18:56:50 +0530

    Done

--- Task1.sql
+++ Task1.sql
@@ -1,44 +1,72 @@
-/*
-Create a table named employees with columns:
-emp_id, name, department, and salary.
+/*List the names and departments of all employees.
+Show all employees who work in the Engineering department.
+Find employees who earn more than 60,000.
+List the names and joining dates of employees who joined before 2019.
+Sort all employees by salary in descending order. */
 
-Insert 4 employee records into the table.
-Show all employees from the Commerce department.
-Show the name and salary of employees who earn more than 25,000.
-Update the salary of the employee with emp_id = 13 to 48,000. 
-*/
+create database emp;
+use emp;
 
-create database  employee_db
-use employee_db;
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    department VARCHAR(50),
+    salary INT,
+    joining_date DATE
+);
 
-/*Create a table named employees with columns:
-emp_id, name, department, and salary.  */
+INSERT INTO employees (emp_id, name, age, department, salary, joining_date)
+VALUES
+(1, 'Alice', 28, 'HR', 50000, '2021-03-15'),
+(2, 'Bob', 35, 'Engineering', 75000, '2019-07-01'),
+(3, 'Charlie', 30, 'Sales', 60000, '2020-01-20'),
+(4, 'David', 45, 'Engineering', 82000, '2018-11-11');
 
-create table employees(
-  emp_id int PRIMARY KEY,
-  name varchar(30),
-  department varchar(30),
-  salary int
-)
 
--- Insert 4 employee records into the table.
 
-insert into employees (emp_id,name,department,salary) values
-(11,"Dhanya","Accounts",25000),
-(12,"Sara","HR",45000),
-(13,"Priya","Commerce",34000),
-(14,"Krish","Sales",20000);
+--1)List the names and departments of all employees.--
+select name, department from employees;
 
--- Show all employees
-select * from employees;
 
--- Show employees from Commerce Department
-select * from employees where department = "Commerce"
+name, department
+Alice	HR
+Bob	Engineering
+Charlie	Sales
+David	Engineering
 
--- Show the name and salary of employees who earn more than 25,000.
-select  name, salary from employees where salary > 25000
 
-select * from employees;
 
--- Update the salary of the employee with emp_id = 13 to 48,000. 
-update employees set salary = 48000 where emp_id=13
+--2)Show all employees who work in the Engineering department---
+select * from employees where department = "Engineering"
+
+
+emp_id, name, age, department, salary, joining_date
+2	Bob	35	Engineering	75000	2019-07-01
+4	David	45	Engineering	82000	2018-11-11
+
+
+
+--3)Find employees who earn more than 60,000. ---
+select * from employees where salary >60000;
+
+emp_id, name, age, department, salary, joining_date
+2	Bob	35	Engineering	75000	2019-07-01
+4	David	45	Engineering	82000	2018-11-11
+
+
+
+--4)List the names and joining dates of employees who joined before 2019.--
+select name,joining_date from employees where joining_date < '2019-01-01'
+
+name, joining_date
+David	2018-11-11
+
+--5)Sort all employees by salary in descending order. --
+select * from employees ORDER BY salary DESC;
+
+emp_id, name, age, department, salary, joining_date
+4	David	45	Engineering	82000	2018-11-11
+3	Charlie	30	Sales	60000	2020-01-20
+2	Bob	35	Engineering	75000	2019-07-01
+1	Alice	28	HR	50000	2021-03-15
